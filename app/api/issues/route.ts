c310a3ec6795c3d31df67f9c579997a5a577c09d
Joetech12 <ifeanyi4umeh@gmail.com>
2024-02-22 17:09:36 +0100

    handled error in create issue page

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,15 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z
+    .string()
+    .min(1, { message: "Title must be 1 or more characters long" })
+    .max(255),
+  description: z
+    .string()
+    .min(1, { message: "Description must be 1 or more characters long" }),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma?.issue.create({
     data: { title: body.title, description: body.description },
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,41 +1,59 @@
 "use client";
 import { useForm, Controller } from "react-hook-form";
-import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+// import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
   description: string;
 }
 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
 const NewIssuePage = () => {
-  const router = useRouter()
+  const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className='max-w-xl space-y-3'
-      onSubmit={handleSubmit(async (data) => {
-       await axios.post("/api/issues", data);
-       router.push('/issues')
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder='Title' {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name='description'
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder='Description' {...field} />
-        )}
-      />
-
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className='max-w-xl'>
+      {error && (
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=' space-y-3'
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            console.log(error);
+            setError("An unexpected error occured");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder='Title' {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name='description'
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder='Description' {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
