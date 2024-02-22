ba3a5e076142de152de58c91d7228cb01a01b0f9
Joetech12 <ifeanyi4umeh@gmail.com>
2024-02-22 19:39:11 +0100

    Added issue details

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingIssueNewPage = () => {
+  return (
+    <div>
+      <p className=''>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingIssueNewPage;
