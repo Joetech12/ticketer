a4134039241ec256ae009d38a320e4781ce55cc4
Joetech12 <ifeanyi4umeh@gmail.com>
2024-02-23 14:35:29 +0100

    Added react markdown and tailwind typography

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import ReactMarkdown from "react-markdown";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
-    
-  if (typeof params.id !== "number") notFound();
+  const pid = parseInt(params.id);
+
+  //   if (typeof pid !== "number") {
+  //     return notFound();
+  //   }
 
   const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: pid },
   });
 
   if (!issue) notFound();
 
+  // console.log({
+  //   type: Number(params.id),
+  //   f: typeof Number(params.id) !== "number",
+  // });
+
   return (
     <div>
-      <p>{issue?.title}</p>
-      <p>{issue?.description}</p>
-      <p>{issue?.status}</p>
-      <p>{issue?.createdAt.toDateString()}</p>
+      <Heading>{issue?.title}</Heading>
+      <Flex gap='3' my='2'>
+        <IssueStatusBadge status={issue?.status} />
+        <Text>{issue?.createdAt?.toDateString()}</Text>
+      </Flex>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issue?.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 };
